c94c8f8b6e81d879dc805714f37be811ed02e056
Cloudmersive <35204726+Cloudmersive@users.noreply.github.com>
2018-06-23 22:01:50 -0700

    Added

--- demo/demo.py
+++ demo/demo.py
@@ -1,7 +1,7 @@
 from __future__ import print_function
 import time
-import cloudmersive_barcode_api_client
-from cloudmersive_barcode_api_client.rest import ApiException
+import cloudmersive_virus_api_client
+from cloudmersive_virus_api_client.rest import ApiException
 from pprint import pprint
 
 
@@ -11,16 +11,18 @@ from pprint import pprint
 
 
 
+
+
 # create an instance of the API class
-api_instance = cloudmersive_barcode_api_client.BarcodeLookupApi()
-value = 'value_example' # str | Barcode value
+api_instance = cloudmersive_virus_api_client.ScanApi()
+input_file = '/path/to/file.txt' # file | Input file to perform the operation on.
 
 api_instance.api_client.configuration.api_key = {}
 api_instance.api_client.configuration.api_key['Apikey'] = '04d1a7be-c9d1-4d93-8ec4-e7545c2a570a'
 
 try:
-    # Lookup a barcode value and return product data
-    api_response = api_instance.barcode_lookup_ean_lookup(value)
+    # Scan a file for viruses
+    api_response = api_instance.scan_file(input_file)
     pprint(api_response)
 except ApiException as e:
-    print("Exception when calling BarcodeLookupApi->barcode_lookup_ean_lookup: %s\n" % e)
+    print("Exception when calling ScanApi->scan_file: %s\n" % e)
