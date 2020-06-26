fc4692eeafab5b62b4c33dd518833eafffc40bf3
Cloudmersive <35204726+Cloudmersive@users.noreply.github.com>
2020-06-25 17:38:58 -0700

    3.0.1

--- build.ps1
+++ build.ps1
@@ -1,5 +1,5 @@
-﻿#Remove-Item –path ./ –recurse
-& java -jar swagger-codegen-cli-2.4.5.jar generate -i https://api.cloudmersive.com/swagger/api/virus -l python -c packageconfig.json
+﻿Remove-Item –path ./cloudmersive_virus_api_client –recurse
+& java -jar swagger-codegen-cli-2.4.14.jar generate -i https://api.cloudmersive.com/swagger/api/virus -l python -c packageconfig.json
 #(Get-Content ./client/package.json).replace('v1', '1.0.1') | Set-Content ./client/package.json
 
 $extrasetup = (Get-Content ./extrasetup.py) -join "`n"
